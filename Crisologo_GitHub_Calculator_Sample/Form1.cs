ca08062a348917dc7bd75b470dd166d2957bff7c
ImNatHarvey <jharveycrisologo@gmail.com>
2026-02-11 19:22:04 +0800

    feat; Layout Done

--- Crisologo_GitHub_Calculator_Sample/Form1.cs
+++ Crisologo_GitHub_Calculator_Sample/Form1.cs
@@ -16,5 +16,20 @@ namespace Crisologo_GitHub_Calculator_Sample
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
